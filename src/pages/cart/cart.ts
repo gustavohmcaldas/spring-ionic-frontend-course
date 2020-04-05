b52eb89b54f8c8fe08aab2f2c7dc958e46be7bba
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-04-04 22:53:28 -0700

    Completing cart functions

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cart-item';
 import { ProductService } from '../../services/domain/product.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProductDTO } from '../../models/product.dto';
 
 @IonicPage()
 @Component({
@@ -22,7 +23,7 @@ export class CartPage {
   }
 
   ionViewDidLoad() {
-    let cart = this.cartService.getCart();
+    let cart = this.cartService.getCart()
     this.items = cart.items;
     this.loadImageUrls();
   }
@@ -32,9 +33,29 @@ export class CartPage {
       let item = this.items[i];
       this.productService.getSmallImageFromBucket(item.product.id)
         .subscribe(response => {
-          item.product.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.product.id}-small.jpg`;
+          item.product.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.product.id}-small.jpg`
         },
         error => {});
     }
   }  
+  
+  removeItem(product: ProductDTO) {
+    this.items = this.cartService.removeProduct(product).items
+  }
+
+  increaseQuantity(product: ProductDTO) {
+    this.items = this.cartService.increaseQuantity(product).items
+  }
+
+  decreaseQuantity(product: ProductDTO) {
+    this.items = this.cartService.decreaseQuantity(product).items
+  }
+
+  total() : number {
+    return this.cartService.total()
+  }  
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriesPage')
+  }
 }
--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -10,26 +10,68 @@ export class CartService {
     }
 
     createOrClearCart() : Cart {
-        let cart: Cart = {items: []};
-        this.storage.setCart(cart);
+        let cart: Cart = {items: []}
+        this.storage.setCart(cart)
         return cart;
     }
 
     getCart() : Cart {
-        let cart: Cart = this.storage.getCart();
+        let cart: Cart = this.storage.getCart()
         if (cart == null) {
-            cart = this.createOrClearCart();
+            cart = this.createOrClearCart()
         }
-        return cart;
+        return cart
     }
 
     addProduct(product: ProductDTO) : Cart {
-        let cart = this.getCart();
-        let position = cart.items.findIndex(x => x.product.id == product.id);
+        let cart = this.getCart()
+        let position = cart.items.findIndex(x => x.product.id == product.id)
         if (position == -1) {
-            cart.items.push({quantity: 1, product: product});
+            cart.items.push({quantity: 1, product: product})
         }
-        this.storage.setCart(cart);
-        return cart;
+        this.storage.setCart(cart)
+        return cart
+    }
+    
+    removeProduct(product: ProductDTO) : Cart {
+        let cart = this.getCart()
+        let position = cart.items.findIndex(x => x.product.id == product.id)
+        if (position != -1) {
+            cart.items.splice(position, 1)
+        }
+        this.storage.setCart(cart)
+        return cart
+    }
+
+    increaseQuantity(product: ProductDTO) : Cart {
+        let cart = this.getCart()
+        let position = cart.items.findIndex(x => x.product.id == product.id)
+        if (position != -1) {
+            cart.items[position].quantity++
+        }
+        this.storage.setCart(cart)
+        return cart
+    }
+
+    decreaseQuantity(product: ProductDTO) : Cart {
+        let cart = this.getCart()
+        let position = cart.items.findIndex(x => x.product.id == product.id)
+        if (position != -1) {
+            cart.items[position].quantity--
+            if (cart.items[position].quantity < 1) {
+                cart = this.removeProduct(product)
+            }
+        }
+        this.storage.setCart(cart)
+        return cart
+    }
+
+    total() : number {
+        let cart = this.getCart()
+        let sum = 0
+        for (var i=0; i<cart.items.length; i++) {
+            sum += cart.items[i].product.price * cart.items[i].quantity
+        }
+        return sum
     }
 }
