b3901e9c030c227c499d41b1dff89841ecaecdc6
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-04-04 21:13:48 -0700

    Loading product images

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProductDTO } from '../../models/product.dto';
 import { ProductService } from '../../services/domain/product.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,19 @@ export class ProductsPage {
     this.productService.findByCategory(category_id)
       .subscribe(response => {
         this.items = response['content']
+        this.loadImageUrls()
       },
       error => {})
   }
+  
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.productService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
+        },
+        error => {});
+    }
+  } 
 }
